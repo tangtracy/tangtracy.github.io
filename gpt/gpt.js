9bb84d31f7dd446497cbb836cd83eeba173cb374
Tracy Tang <tracytanggg@gmail.com>
2023-04-23 16:16:43 -0400

    add api key

--- gpt/gpt.js
+++ gpt/gpt.js
@@ -1,15 +1,48 @@
-document.getElementById('chat-form').addEventListener('submit', async () => {
-    
+const chatOutput = document.getElementById('chat-output');
+const typingDelay = 30; // Delay between each character being typed
+
+// Function to add a message to the chat output with typing animation
+async function addMessageWithTypingAnimation(sender, message) {
+    const output = document.createElement('div');
+    output.innerHTML = `<strong>${sender}:</strong> `;
+    chatOutput.appendChild(output);
+
+    for (let i = 0; i < message.length; i++) {
+        await delay(typingDelay); // Delay before typing next character
+        output.innerHTML += message.charAt(i);
+    }
+
+    chatOutput.scrollTop = chatOutput.scrollHeight; // Scroll to bottom of chat output
+}
+
+// Function to add a message to the chat output without typing animation
+async function addMessageWithoutTypingAnimation(sender, message) {
+    const output = document.createElement('div');
+    output.innerHTML = `<strong>${sender}:</strong> `;
+    chatOutput.appendChild(output);
+
+    output.innerHTML += message
+    chatOutput.scrollTop = chatOutput.scrollHeight; // Scroll to bottom of chat output
+}
+
+// Function to delay for a given amount of time
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Start message
+addMessageWithTypingAnimation('AI', "Hi :-) I'm a GPT powered chat bot. Ask me a question about Tracy's site!");
+
+document.getElementById('chat-form').addEventListener('submit', async (event) => {
     event.preventDefault();
 
     const chatInput = document.getElementById('chat-input');
-    const chatOutput = document.getElementById('chat-output');
-    
+
     let message = chatInput.value;
     chatInput.value = '';
 
-    // Add user message to chat output
-    chatOutput.innerHTML += `<div><strong>You:</strong> ${message}</div>`;
+    // Add user message to chat output with typing animation
+    await addMessageWithoutTypingAnimation('You', message);
 
     message = `Tracy Tang
     tracy-tang.com 
@@ -33,6 +66,8 @@ document.getElementById('chat-form').addEventListener('submit', async () => {
     Mentored students, recruited students, and  judged hacks, for a grand prize of interviews for best financial literacy, at HackHersー a female, femme, and non-binary hackathon for Rutgers University students 
     Developed engagement activities and a mindfulness session for the Technology Leadership Program participants, encouraging a positive and energetic work environment 
     
+    I am now in my second rotation of TLP on the Desk Development team. We build technology tools and solutions for traders. 
+
     Software Engineering Intern - Collins Aerospace 
     Cedar Rapids, IA: June 2021-August 2021 
     Used Python to query for employees’ names and display them on a disk usage site 
@@ -46,35 +81,18 @@ document.getElementById('chat-form').addEventListener('submit', async () => {
     Developed a Bash script to perform any number of OpenSCAP scans on a local system simultaneously 
     Used Python scripting to manipulate and merge two XML datastreams 
     Researched and tested a third-party security vulnerability scanner plug-in for its effectiveness as an integrated component of the team’s CI/CD pipeline 
-    
-    ECE 391 (Operating Systems course) - Final Project
-    Champaign, IL: March 2021-May 2021
-    Collaborated in a team of four students to create an operating system over the course of three months 
-    Implemented a real time clock device, physical to virtual memory paging, interrupt descriptor table, terminal functionality, and kernel program loading with teammates 
-    Personally developed a read-only file system, and kernel system calls to support user programs 
-    
-    CS 225 (Data Structures) - Final Project 
-    Champaign, IL: November 2020-December 2020 
-    Used C++ to create a Force-Directed graph of a dataset of physics authors and their co-authors 
-    Used BFS and Dijkstra traversals to gather information about the nodes representing collaborations 
-    
-    I'm an application developer at Vanguard, currently in my second rotation in the Technology Leadership Program.
 
-    contact me: 630-729-4928, philadelphia, PA, tracytanggg@gmail.com
-
-    interests: hiking, music, emotional literacy, true crime, plants
-    ` + 'use the above resume to answer the following question: ' +  message
+    ` + 'use the above resume to answer the following question: ' + message;
 
     // Call OpenAI API
     const response = await fetchOpenAiApi(message);
 
-    // Add AI response to chat output
-    chatOutput.innerHTML += `<div><strong>AI:</strong> ${response}</div>`;
-    chatOutput.scrollTop = chatOutput.scrollHeight;
+    // Add AI response to chat output with typing animation
+    await addMessageWithTypingAnimation('AI', response);
 });
 
 async function fetchOpenAiApi(message) {
-    const apiKey = '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'; // Replace with your OpenAI API key
+    const apiKey = process.env.MY_OPENAI_KEY; // Replace with your OpenAI API key
     const apiUrl = 'https://api.openai.com/v1/chat/completions'; // Modify this URL according to the API version and desired engine
 
     const requestOptions = {
@@ -89,10 +107,7 @@ async function fetchOpenAiApi(message) {
         })
     };
 
-
     const response = await fetch(apiUrl, requestOptions);
     const data = await response.json();
-    console.log(data)
     return data.choices[0].message.content;
-    // return 'hi'
 }
